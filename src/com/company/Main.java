4c2c0bcbd11c46bf3b3fedd24a8828f4313e8e02
Nick <lanikolya1989@gmail.com>
2017-12-25 16:37:03 +0200

    Fixed name of check method Added description for methods

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,23 +12,38 @@ public class Main {
         restart(alphabet,index);
     }
 
+    /**
+     * Create alphabet
+     * @return array with alphabet
+     */
     public static String[] arrayABC() {
         String[] ABC = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
         return ABC;
     }
 
+    /**
+     * Choose an index of winning letter
+     * @return a pseudorandom {@code int} from 0 to 25 which is equivalent of letter index from alphabet
+     */
     public static int indexOfWinningLetter() {
-        int r = (int) (Math.random() * 26);
-        int i = r;
-        return i;
+        return (int) (Math.random() * 26);
     }
 
+    /**
+     * Enter of a letter by user
+     */
     public static String enter() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a letter");
         return scanner.next();
     }
 
+    /**
+     * Compute the index of the letter from user
+     * @param letterFromUser letter which was entered by user
+     * @param alphabet array with all letters of alphabet
+     * @return index of entered letter (int from 0 to 25) or -1 if user has entered incorrect symbol
+     */
     public static int indexOfLetterFromUser(String letterFromUser, String[] alphabet) {
         for (int i = 0; i < alphabet.length; i++) {
             if (letterFromUser.equals(alphabet[i])) {
@@ -38,7 +53,10 @@ public class Main {
         return -1;
     }
 
-    public static void chek(String letterFromUser, String[] alphabet, int index) {
+    /**
+     * Check user answer and display an appropriate message
+     */
+    public static void check(String letterFromUser, String[] alphabet, int index) {
         int usersIndex = indexOfLetterFromUser(letterFromUser, alphabet);
         if (usersIndex == index) {
             System.out.println("You are win");
@@ -54,7 +72,10 @@ public class Main {
         }
     }
 
+    /**
+     * Restart check method
+     */
     public static void restart(String[] alphabet, int index) {
-        chek(enter(), alphabet, index);
+        check(enter(), alphabet, index);
     }
 }
