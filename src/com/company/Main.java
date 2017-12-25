6b35bd57ea6e2a77c65c5f2e841f2f18b36c9967
Nick <lanikolya1989@gmail.com>
2017-12-25 17:19:00 +0200

    Deleted redundant code Used loop while to restart app when user enter incorrect answer

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,21 +8,24 @@ public class Main {
         String[] alphabet = arrayABC();
         int index = indexOfWinningLetter();
         String winningLetter = alphabet[index];
+        //Display of winning letter to make checking of the work of app easier
         System.out.println(winningLetter);
-        restart(alphabet,index);
+        check(alphabet, index);
     }
 
     /**
      * Create alphabet
+     *
      * @return array with alphabet
      */
     public static String[] arrayABC() {
-        String[] ABC = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
-        return ABC;
+        return new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+                        "s", "t", "u", "v", "w", "x", "y", "z"};
     }
 
     /**
      * Choose an index of winning letter
+     *
      * @return a pseudorandom {@code int} from 0 to 25 which is equivalent of letter index from alphabet
      */
     public static int indexOfWinningLetter() {
@@ -40,8 +43,9 @@ public class Main {
 
     /**
      * Compute the index of the letter from user
+     *
      * @param letterFromUser letter which was entered by user
-     * @param alphabet array with all letters of alphabet
+     * @param alphabet       array with all letters of alphabet
      * @return index of entered letter (int from 0 to 25) or -1 if user has entered incorrect symbol
      */
     public static int indexOfLetterFromUser(String letterFromUser, String[] alphabet) {
@@ -56,26 +60,19 @@ public class Main {
     /**
      * Check user answer and display an appropriate message
      */
-    public static void check(String letterFromUser, String[] alphabet, int index) {
-        int usersIndex = indexOfLetterFromUser(letterFromUser, alphabet);
-        if (usersIndex == index) {
-            System.out.println("You are win");
-        } else if (usersIndex > index) {
-            System.out.println("Try once more,the letter above");
-            restart(alphabet,index);
-        } else if (usersIndex < index&&usersIndex!=-1) {
-            System.out.println("Try once more,the letter below");
-            restart(alphabet,index);
-        } else if (usersIndex == -1) {
-            System.out.println("Incorrect symbol, enter a letter");
-            restart(alphabet,index);
+    public static void check(String[] alphabet, int index) {
+        while (true) {
+            int usersIndex = indexOfLetterFromUser(enter(), alphabet);
+            if (usersIndex == index) {
+                System.out.println("You are win");
+                break;
+            } else if (usersIndex > index) {
+                System.out.println("Try once more,the letter above");
+            } else if (usersIndex < index && usersIndex != -1) {
+                System.out.println("Try once more,the letter below");
+            } else if (usersIndex == -1) {
+                System.out.println("Incorrect symbol, enter a letter");
+            }
         }
-    }
-
-    /**
-     * Restart check method
-     */
-    public static void restart(String[] alphabet, int index) {
-        check(enter(), alphabet, index);
     }
 }
